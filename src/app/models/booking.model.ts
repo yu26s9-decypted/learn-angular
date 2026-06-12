1f48c816698f3660d41fbe25cfb92d495321f16d
andy <andyt.cyb@gmail.com>
2026-06-12 12:36:41 -0400

    added concert & booking model

---
+++ src/app/models/booking.model.ts
@@ -0,0 +1,7 @@
+
+
+export interface Booking{
+    id: number;
+    customerName: string;
+    customerEmail: string;
+}
---
+++ src/app/models/concert.model.ts
@@ -0,0 +1,11 @@
+
+export interface Concert{
+    id: number;
+    title: string;
+    date: string;
+    artist: null; //todo
+    venue: null; // todo
+    totalSeats: number;
+    availableSeats: number;
+    ticketPrice: number;
+}
